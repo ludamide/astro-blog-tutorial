bbebe67bee107c361c9454de6e6c5844851277e6
Ludvig Amide <ludvig.amide@outlook.es>
2024-01-11 17:49:29 +0100

    Blog finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://magnificent-licorice-7936e9.netlify.app/'
+  site: 'https://magnificent-licorice-7936e9.netlify.app/',
+  integrations: [preact()]
 });
